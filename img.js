bcde016f77d19412629cf569af45a4d15d887454
mijung858 <125540704+mijung858@users.noreply.github.com>
2024-02-07 11:01:02 +0900

    네임태그 진도

--- img.js
+++ img.js
@@ -1,3 +1,53 @@
+// $(document).ready(function () {
+//     $(".single-item").slick();
+
+//     var strawberryCake = "img/cake.png";
+//     var CherryHam = "img/cherryHam.png";
+//     var StrawberryHam = "img/strawberryham.png";
+//     var dish = "img/dish.png";
+
+//     var selectedItems = {
+//         myImg1: false,
+//         myImg2: false,
+//         myImg3: false,
+//         myImg4: false,
+//         myImg5: false,
+//         myImg6: false,
+//         myImg7: false,
+//         myImg8: false,
+//         myImg9: false,
+//     };
+
+//     $(".box img").click(function () {
+//         var id = $(this).attr("id");
+//         selectedItems[id] = !selectedItems[id];
+//         toggleSetImage();
+//         $(this).toggleClass("clicked"); // Add or remove the "clicked" class
+//     });
+
+//     function toggleSetImage() {
+//         var allSelected1 = selectedItems.myImg1 && selectedItems.myImg2 && selectedItems.myImg3;
+//         var allSelected2 = selectedItems.myImg4 && selectedItems.myImg5 && selectedItems.myImg6;
+//         var allSelected3 = selectedItems.myImg1 && selectedItems.myImg2 && selectedItems.myImg6;
+
+//         $("#stagezone").empty(); // Remove existing images
+
+//         if (allSelected1) {
+//             $("#stagezone").append(`<img src='${strawberryCake}' alt=""style="top:-25px;">`);
+//         } else if (allSelected2) {
+//             $("#stagezone").append(`<img src='${CherryHam}' alt=""style="top:-15px;">`);
+//         } else if (allSelected3) {
+//             $("#stagezone").append(`<img src='${StrawberryHam}' alt=""style="top:-25px;">`);
+//         } else {
+//             $("#stagezone").append(`<img src='img/bowl.png' alt="">`);
+//         }
+
+//         if (allSelected1 || allSelected2 || allSelected3) {
+//             $("#stagezone").append(`<img src='${dish}' alt="" style="z-index: -10;">`);
+//         }
+//     }
+// });
+
 $(document).ready(function () {
     $(".single-item").slick();
 
@@ -5,7 +55,7 @@ $(document).ready(function () {
     var CherryHam = "img/cherryHam.png";
     var StrawberryHam = "img/strawberryham.png";
     var dish = "img/dish.png";
-
+    var nameTag = "img/nametag.png";
     var selectedItems = {
         myImg1: false,
         myImg2: false,
@@ -33,17 +83,26 @@ $(document).ready(function () {
         $("#stagezone").empty(); // Remove existing images
 
         if (allSelected1) {
-            $("#stagezone").append(`<img src='${strawberryCake}' alt="">`);
+            $("#stagezone").append(`<img src='${strawberryCake}' alt="" style="top: -25px;">`);
         } else if (allSelected2) {
-            $("#stagezone").append(`<img src='${CherryHam}' alt="">`);
+            $("#stagezone").append(`<img src='${CherryHam}' alt="" style="top: -15px;">`);
         } else if (allSelected3) {
-            $("#stagezone").append(`<img src='${StrawberryHam}' alt="">`);
+            $("#stagezone").append(`<img src='${StrawberryHam}' alt="" style="top: -25px;">`);
         } else {
             $("#stagezone").append(`<img src='img/bowl.png' alt="">`);
         }
 
         if (allSelected1 || allSelected2 || allSelected3) {
-            $("#stagezone").append(`<img src='${dish}' alt="">`);
+            $("#stagezone").append(`<img src='${dish}' alt="" style="z-index: -10;">`);
+        }
+        if (allSelected1 || allSelected2 || allSelected3) {
+            $("#stagezone").append(`<img src='${nameTag}' alt="" style="z-index: 10;     top: 360px;" >`);
+        }
+
+        // Check if three or more images are selected
+        var selectedCount = Object.values(selectedItems).filter(Boolean).length;
+        if (selectedCount >= 4) {
+            alert("네가지 재료만 선택해줘!");
         }
     }
 });
